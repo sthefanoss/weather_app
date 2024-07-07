4387bda07923e4bec82e23652bc485138461105d
sthefano <sthefanoss@gmail.com>
2024-07-06 21:20:03 -0300

    feat(CurrentWeatherPage): display data from controller

--- lib/pages/current_weather_page.dart
+++ lib/pages/current_weather_page.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:weather_app/controllers/controller_state.dart';
 import 'package:weather_app/controllers/current_weather_controller.dart';
 
 class CurrentWeatherBindings extends Bindings {
@@ -37,20 +39,103 @@ class _CurrentWeatherPageState extends State<CurrentWeatherPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Current Weather')),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Text(
-                controller.location,
-                style: Theme.of(context).textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
-              ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Text(
+              controller.location,
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 16),
-          ],
-        ),
+          ),
+          Obx(() {
+            final state = controller.state.value;
+
+            if (state is ErrorState) {
+              return Column(
+                children: [
+                  const Text('There was an error, please try again.'),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: controller.fetchCurrentWeather,
+                    child: const Text('Retry'),
+                  )
+                ],
+              );
+            }
+
+            if (state case SuccessState(:final data)) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    WeatherInfo(
+                      title: 'Temperature',
+                      value: data.temperature,
+                      unit: '°C',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    Wrap(
+                      children: [
+                        for (final weather in data.weather)
+                          Column(
+                            children: [
+                              CachedNetworkImage(
+                                imageUrl: weather.iconUrl,
+                                width: 128,
+                                height: 128,
+                                fit: BoxFit.cover,
+                              ),
+                              Text(weather.description.capitalize!),
+                            ],
+                          )
+                      ],
+                    ),
+                    const SizedBox(height: 24),
+                    WeatherInfo(title: 'Rain', value: data.rain, unit: 'mm'),
+                    WeatherInfo(title: 'Snow', value: data.snow, unit: 'mm'),
+                    WeatherInfo(title: 'Humidity', value: data.humidity, unit: '%'),
+                    WeatherInfo(title: 'Wind', value: data.windSpeed, unit: 'm/s'),
+                  ],
+                ),
+              );
+            }
+
+            return const Center(child: CircularProgressIndicator());
+          }),
+        ],
+      ),
+    );
+  }
+}
+
+class WeatherInfo extends StatelessWidget {
+  final String title;
+  final num value;
+  final String unit;
+  final TextStyle? style;
+
+  const WeatherInfo({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.unit,
+    this.style,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final effectiveStyle = style ?? Theme.of(context).textTheme.bodyLarge;
+    return RichText(
+      text: TextSpan(
+        style: effectiveStyle,
+        children: [
+          TextSpan(text: '$title: ', style: effectiveStyle?.copyWith(fontWeight: FontWeight.bold)),
+          TextSpan(text: '$value'),
+          TextSpan(text: unit, style: effectiveStyle?.copyWith(fontWeight: FontWeight.w300)),
+        ],
       ),
     );
   }
