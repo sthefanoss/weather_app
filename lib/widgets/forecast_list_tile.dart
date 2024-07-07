7d569f15fd055a2ca8e29d8718c3832c7a75541b
sthefano <sthefanoss@gmail.com>
2024-07-07 20:22:46 -0300

    chore(ForecastListTile): improve responsiveness

--- lib/widgets/forecast_list_tile.dart
+++ lib/widgets/forecast_list_tile.dart
@@ -11,32 +11,45 @@ class ForecastListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Stack(
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Row(
         children: [
-          CachedNetworkImage(
-            imageUrl: forecastEntry.weather.first.iconUrl,
-            width: 48,
-            height: 48,
-            fit: BoxFit.cover,
+          Expanded(
+            flex: 3,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CachedNetworkImage(
+                  imageUrl: forecastEntry.weather.first.iconUrl,
+                  width: 48,
+                  height: 48,
+                  fit: BoxFit.cover,
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Text(DateFormat('dd/MM\nHH:mm').format(forecastEntry.timestamp)),
+                ),
+              ],
+            ),
           ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Text(DateFormat('dd/MM HH:mm').format(forecastEntry.timestamp)),
+          Expanded(
+            flex: 7,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(forecastEntry.weather.first.description.capitalize!,
+                    style: Theme.of(context).textTheme.titleLarge),
+                WeatherInfo(title: 'Temperature', value: forecastEntry.temperature, unit: '°C'),
+                WeatherInfo(title: 'Rain', value: forecastEntry.rain, unit: 'mm'),
+                WeatherInfo(title: 'Snow', value: forecastEntry.snow, unit: 'mm'),
+                WeatherInfo(title: 'Humidity', value: forecastEntry.humidity, unit: '%'),
+                WeatherInfo(title: 'Wind', value: forecastEntry.windSpeed, unit: 'm/s'),
+              ],
+            ),
           ),
-        ],
-      ),
-      title: Text(forecastEntry.weather.first.description.capitalize!),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          WeatherInfo(title: 'Temperature', value: forecastEntry.temperature, unit: '°C'),
-          WeatherInfo(title: 'Rain', value: forecastEntry.rain, unit: 'mm'),
-          WeatherInfo(title: 'Snow', value: forecastEntry.snow, unit: 'mm'),
-          WeatherInfo(title: 'Humidity', value: forecastEntry.humidity, unit: '%'),
-          WeatherInfo(title: 'Wind', value: forecastEntry.windSpeed, unit: 'm/s'),
         ],
       ),
     );
